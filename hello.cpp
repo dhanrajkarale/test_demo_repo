092297421a14e1b644bd4324db5272846aa88498
Dhanraj Karale <84983455+dk-1311@users.noreply.github.com>
2023-01-22 13:05:37 +0530

    second commit

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,7 @@ int main()
     cout << "Demo git repository";
     cout<<"This is the first github practical";
 
-    cout<<"undo commited file";
+    cout<<"undo commited file"; 
+    cout<<"undo commited file 2"; 
     return 0;
 }
