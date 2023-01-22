44b2647b99a5bfbecd0403d6ad44ac68305790d0
Dhanraj Karale <84983455+dk-1311@users.noreply.github.com>
2023-01-22 13:06:29 +0530

    Revert "second commit"
    This reverts commit 092297421a14e1b644bd4324db5272846aa88498.

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,6 @@ int main()
     cout << "Demo git repository";
     cout<<"This is the first github practical";
 
-    cout<<"undo commited file"; 
-    cout<<"undo commited file 2"; 
+    cout<<"undo commited file";
     return 0;
 }
