d3c5b7459aaf7f7017cf78fadaafd2cede1d82cd
Dhanraj Karale <84983455+dk-1311@users.noreply.github.com>
2023-01-23 12:44:39 +0530

    clg add

--- hello.cpp
+++ hello.cpp
@@ -6,8 +6,11 @@ int main()
     cout << "Demo git repository";
     cout<<"This is the first github practical";
 
-    cout<<"undo commited file";
+    cout<<"undo commited file"; 
 
     cout<<"hello is the new branch";
+
+    cout<<"Sinhgad College of engieering";
+    cout<<"pune";
     return 0;
 }
